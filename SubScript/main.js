05d5a658cd75d101eb82abf58b97e80ac5c866c3
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2021-06-28 14:07:49 -0500

    Terminado 28 Junio 2021

--- SubScript/main.js
+++ SubScript/main.js
@@ -1,64 +1,138 @@
-//Crear un formulario con los siguientes campos
-//Tipo de documento, Cedula, Nombre, Apellido, edad, genero,email,telefono, celular, mensaje,
-//id, type, name, enable, for,
-//Crear los nodos por medio del JavaScript
-//Nota utilizar el metodo open(); para la creacion de la ventana
 let myFormulario = {
     Create: function (){
         let fragmen = new DocumentFragment();
+        let form = document.createElement("FORM");
+        let myStyle = {
+            display: "block",
+            margin: "10px auto",
+            width: "45%",
+            height: "30px"
+        }
+        form.id = "myForm";
+        fragmen.append(form);
         for (let i = 0; i < this.data.length; i++) {
-            let nodo = document.createElement(""+this.data[i].tag);
+            let nodo = document.createElement(this.data[i].tag);
             for(const datos in this.data[i]){
-                nodo[datos] = this.data[i][datos];
+                if(this.data[i].tag=="INPUT"){
+                    nodo[datos] = this.data[i][datos];
+                    Object.assign(nodo.style, myStyle);
+                }else if(this.data[i].tag=="TEXTAREA"){
+                    // https://www.delftstack.com/es/howto/javascript/javascript-remove-property-from-object/#use-la-sintaxis-spread-para-eliminar-una-propiedad-de-un-objeto-en-javascript-ecmascript-6
+                    var {height, ...myStyleUpdated} = myStyle;
+                    nodo[datos] = this.data[i][datos];
+                    Object.assign(nodo.style, myStyleUpdated);
+                }else if(this.data[i].tag=="SELECT"){
+                    nodo[datos] = this.data[i][datos];
+                    let opcionesFragment = new DocumentFragment();
+                    for(const select in this.data[i].option){
+                        let opciones = document.createElement("OPTION");
+                        opciones.value = this.data[i].option[select].value;
+                        let texto = document.createTextNode(this.data[i].option[select].text);
+                        opciones.appendChild(texto);
+                        opcionesFragment.appendChild(opciones);
+                        delete this.data[i].option[select];
+                    }
+                    Object.assign(nodo.style, myStyle);
+                    nodo.appendChild(opcionesFragment);
+                }
+                
             }
-            fragmen.appendChild(nodo);
+            fragmen.children.myForm.append(nodo);
         }
         document.body.appendChild(fragmen);
     },
 }
-
 let propiedades = Object.create(myFormulario);
 propiedades.data = [
     {
+        tag: "SELECT",
+        id: "mySelect1",
+        name: "mySelect1",
+        option:[
+            {
+                value: "",
+                text: "Tipo de documento"
+            },
+            {
+                value: "TI",
+                text: "Targeta de identidad"
+            },
+            {
+                value: "CC",
+                text: "Cedula de ciudadania"
+            }
+        ]
+    },
+    {
         tag: "INPUT",
         id: "myInput1",
-        type: "text",
+        type: "number",
         name: "myInput1",
-        placeholder: "Nombre Completo",
+        placeholder: "N° Documento",
     },
     {
         tag: "INPUT",
         id: "myInput2",
         type: "text",
         name: "myInput2",
-        placeholder: "Apellido Completo",
+        placeholder: "Nombre Completo",
     },
     {
         tag: "INPUT",
         id: "myInput3",
-        type: "number",
+        type: "text",
         name: "myInput3",
-        placeholder: "Edad",
+        placeholder: "Apellido Completo",
     },
     {
         tag: "INPUT",
         id: "myInput4",
-        type: "email",
+        type: "number",
         name: "myInput4",
-        placeholder: "Correo electronico",
+        placeholder: "Edad",
+    },
+    {
+        tag: "SELECT",
+        id: "mySelect2",
+        name: "mySelect2",
+        option:[
+            {
+                value: "",
+                text: "Genero"
+            },
+            {
+                value: "M",
+                text: "Masculino"
+            },
+            {
+                value: "F",
+                text: "Femenino"
+            },
+            {
+                value: "N",
+                text: "Prefiero no decirlo"
+            }
+        ]
     },
     {
         tag: "INPUT",
         id: "myInput5",
-        type: "number",
+        type: "email",
         name: "myInput5",
-        placeholder: "Telefono",
+        placeholder: "Correo electronico",
     },
     {
         tag: "INPUT",
         id: "myInput6",
         type: "number",
         name: "myInput6",
+        placeholder: "Telefono",
+    },
+    {
+        tag: "INPUT",
+        id: "myInput7",
+        type: "number",
+        name: "myInput7",
         placeholder: "Celular",
     },
     {
@@ -68,6 +142,13 @@ propiedades.data = [
         cols: "30",
         rows: "10",
         placeholder: "Mensaje",
+    },
+    {
+        tag: "INPUT",
+        id: "myBoton",
+        type: "submit",
+        name: "myBoton",
+        value: "Enviar Datos",
     },
 ]
 propiedades.Create();
